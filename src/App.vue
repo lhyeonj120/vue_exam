35819f5a7426ca06b1505ea7005a96ba6c5351d2
lhyeonj120 <isk04387@gmail.com>
2023-03-16 09:31:56 +0900

    card template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container"> <!-- 중간정렬 -->
+    <h2>To-Do</h2>
+    <form 
+      @submit.prevent="onSubmit"  
+    >
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input 
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          > 
+        </div>
+
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div v-if="hasError" style="color:red">This is cannot be empty</div>
+    </form>
+    <div
+      v-for="todo in todos"
+      :key="todo.id" 
+      class="card mt-2">
+      <div class="card-body p-2">
+        <div class="form-check">
+          <input class="form-check-input"
+          type="checkbox">
+          <label class="form-check-label">
+            {{todo.subject}}
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const todo = ref('');
+    const todos = ref([
+      {id:1, subject:'study'},
+      {id:2, subject:'house'}
+    ]);
+
+    const hasError = ref(false);
+
+    const onSubmit = () => {
+      if(todo.value == ''){
+        hasError.value = true;
+      }else{
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value
+        }); //객체 형태로 넣기
+        hasError.value = false;
+      }
+      
+    }
+
+    return{
+      todo, 
+      onSubmit,
+      todos,
+      hasError,
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .name{
+    color: red;
+  }
 </style>
