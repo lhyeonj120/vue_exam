3c38e742bd28c05222f5d591b9ff1abfdafa4348
lhyeonj120 <isk04387@gmail.com>
2023-03-16 10:18:20 +0900

    modify delete button

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,8 @@
       v-for="todo in todos"
       :key="todo.id" 
       class="card mt-2">
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input class="form-check-input"
             type="checkbox"
             v-model="todo.completed">
@@ -38,11 +38,11 @@
             :class="{todo: todo.completed}">
             {{todo.subject}}
           </label>
-          <div>
-            <button class="btn btn-danger btn-sm">
-              Delete
-            </button>
-          </div>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm">
+            Delete
+          </button>
         </div>
       </div>
     </div>
